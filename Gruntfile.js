17c0d6d85824f58d717d2d296e059a063daf6073
Denis P <denisvad1313@rambler.ru>
2015-03-17 15:58:26 +0200

    start paymentWidget

---
+++ Gruntfile.js
@@ -0,0 +1,184 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+
+		stylus: {
+			options: {
+				compress: false
+			},
+			compile: {
+				files: [{
+					cwd: 'app/styles',
+					src: 'common.styl',
+					dest: 'dist/styles',
+					expand: true,
+					ext: '.css'
+				}]
+			}
+		},
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                force: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            all: [
+                'app/scripts/**/*.js',
+                '!app/scripts/libs/**/*'
+            ],
+            configFiles: [
+                'Gruntfile.js',
+                'package.json'
+            ]
+        },
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'Android >= <%= pkg.browsers.android %>',
+                    'Chrome >= <%= pkg.browsers.chrome %>',
+                    'Firefox >= <%= pkg.browsers.firefox %>',
+                    'Explorer >= <%= pkg.browsers.ie %>',
+                    'iOS >= <%= pkg.browsers.ios %>',
+                    'Opera >= <%= pkg.browsers.opera %>',
+                    'Safari >= <%= pkg.browsers.safari %>'
+                ]
+            },
+            dist: {
+                src: ['dist/styles/**/*.css']
+            }
+        },
+        copy: {
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/images',
+                    src: ['*.*'],
+                    dest: 'dist/images',
+                    filter: 'isFile'
+                }]
+            },
+            html: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/html',
+                    src: '*',
+                    dest: 'dist',
+                    filter: 'isFile'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/fonts',
+                    src: '*',
+                    dest: 'dist/fonts',
+                    filter: 'isFile'
+                }]
+            },
+            scripts: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/scripts',
+                    src: ['**/*.js'],
+                    dest: 'dist/scripts',
+                    filter: 'isFile'
+                }]
+            }
+        },
+        imagemin: {
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/images',
+                    src: ['**/*.{png,jpg,gif}', '!sprite/**/*'],
+                    dest: 'dist/images'
+                }]
+            }
+        },
+        sprite: {
+            all: {
+                src: 'app/images/sprite/*.png',
+                dest: 'app/images/sprite.png',
+                destCss: 'app/styles/__helpers/sprite.styl',
+                imgPath : '../images/sprite.png',
+                cssFormat: 'stylus',
+                algorithm: 'binary-tree',
+                padding: 8,
+                imgOpts: {
+                    format: 'png'
+                }
+            }
+        },
+        watch: {
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: ['dist/**/*']
+            },
+            configFiles: {
+                options: {
+                    reload: true
+                },
+                files: ['Gruntfile.js', 'package.json'],
+                tasks: ['newer:jshint:configFiles']
+            },
+            stylus: {
+                files: ['app/styles/**/*.styl'],
+                tasks: ['stylus']
+            },
+            jshint: {
+                files: ['app/scripts/**/*.js'],
+                tasks: ['newer:jshint:all']
+            },
+            imagemin: {
+                files: ['app/images/**/*.{png,jpg,gif}', '!app/images/sprite/**/*'],
+                tasks: ['newer:imagemin']
+            },
+            html : {
+                files: ['app/html/**/*.html'],
+                tasks: ['newer:copy:html']
+            },
+            sprite: {
+                files: ['app/images/sprite/**/*.png'],
+                tasks: ['sprite']
+            },
+            scripts: {
+                files: ['app/scripts/**/*.js', '!app/scripts/libs/**/*'],
+                tasks: ['newer:copy:scripts']
+            },
+            copyFonts: {
+                files: ['app/fonts/*'],
+                tasks: ['newer:copy:fonts']
+            },
+        }
+	});
+
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', [
+        'clean',
+        'stylus',
+        'sprite',
+        'jshint',
+        'autoprefixer',
+        'copy',
+        'imagemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'build',
+        'watch',
+    ]);
+};
---
+++ app/scripts/all.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    /*scrollbar*/
+    $(".j-scroll").mCustomScrollbar();
+
+    /*rollup*/
+    $('.j-pkp-rollup__trigger').on('click', function(e){
+        $(this).toggleClass('pkp-rollup__trigger_closed').parent().find('.j-pkp-rollup__content').slideToggle();
+        e.preventDefault();
+    });
+    
+    /*select*/
+    $("select").selecter({
+        label: "По умолчанию"
+    });
+
+    $('.j-pkp-payment').on('submit', function(){
+        var form = $(this);
+        var input = form.find('.j-pkp-req-input');
+        var pattern = /\w+@\w+\.\w{2,6}/gi;
+        var errorMsg = input.siblings('.pkp-error-msg'); 
+        if(input.val().length === 0) {
+            input.addClass('pkp-input_error');
+            errorMsg.text('Заполните поле').fadeIn();
+            return false;
+        }  
+        if(!pattern.test(input.val())) {
+            input.addClass('pkp-input_error');
+            errorMsg.text('Обязательное поле').fadeIn();
+            return false;
+        }
+        if(pattern.test(input.val())) {
+            input.removeClass('pkp-input_error');
+            errorMsg.fadeOut();
+            return false;
+        }
+    });  
+
+    /* tooltips */
+    function toolTips() {
+        if($('.pkp-container').innerWidth() > 768){
+            $('.pkp-payment-method .pkp-payment-method__item:nth-child(3n + 1)').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_left');
+            });
+            $('.pkp-payment-method .pkp-payment-method__item:nth-child(3n + 2)').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_center');
+            });
+            $('.pkp-payment-method .pkp-payment-method__item:nth-child(3n + 3)').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_right');
+            });
+        }
+
+        if($('.pkp-container').innerWidth() < 768 && $('.pkp-container').innerWidth() > 578){
+            $('.pkp-payment-method .pkp-payment-method__item:nth-child(2n+1)').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_left');
+            });
+            $('.pkp-payment-method .pkp-payment-method__item:nth-child(2n)').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_right');
+            });
+        }
+
+        if($('.pkp-container').innerWidth() < 578){
+            $('.pkp-payment-method .pkp-payment-method__item').each(function(){
+                $(this).find('.pkp-tooltip__content').removeAttr('class').addClass('pkp-tooltip__content pkp-tooltip__content_center');
+            });
+        }
+    }
+    toolTips();
+    $(window).on('resize', function(){
+        toolTips();
+    });
+
+});
